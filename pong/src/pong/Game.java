29d3c5c19fa20b6814c467872aeba45247ff0894
Darlan Bueno <darlan_bueno@hotmail.com>
2019-07-19 02:38:29 -0300

    criada a classe player, onde damos vida ao nosso retangulo, que ao pressionar os botões da esquerda ou direita, conseguimos movimenta-lo. Ele também foi limitado às beiradas da tela.

---
+++ pong/src/pong/Game.java
@@ -0,0 +1,113 @@
+package pong;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
+
+
+
+    
+    public class Game extends Canvas implements Runnable,KeyListener{
+    
+    public static int WIDTH = 240;
+    public static int HEIGHT = 120;
+    public static int SCALE = 3;
+    
+    public BufferedImage layer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    
+    public Player player;
+    
+    public Game(){
+        
+        this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        this.addKeyListener(this);
+        player = new Player(100,HEIGHT-10);
+        
+    }
+    
+    public static void main(String[] args){   
+        Game game = new Game();
+        JFrame frame = new JFrame("Pong");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.add(game);
+        frame.pack();
+        frame.setVisible(true);
+        
+        new Thread (game).start();
+        
+    }
+    
+    public void tick(){
+        player.tick();
+        
+    }
+       public void render(){
+           
+           BufferStrategy bs = this.getBufferStrategy();
+           if (bs == null) {
+               this.createBufferStrategy(3);
+               return;
+           }
+           Graphics g = layer.getGraphics();
+           g.setColor(Color.black);
+           g.fillRect(0, 0, WIDTH, HEIGHT);
+           player.render(g);
+           
+           
+           g = bs.getDrawGraphics();
+           g.drawImage(layer, 0, 0,WIDTH*SCALE, HEIGHT*SCALE,null);
+           bs.show();
+    }
+    
+    
+    public void run(){
+        while (true) {   
+            tick();
+            render();
+            try {
+                Thread.sleep(1000/60);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+               
+        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+     
+       
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+         if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            player.right = true;
+        }else if (e.getKeyCode() == KeyEvent.VK_LEFT){
+            player.left = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            player.right = false;
+        }else if (e.getKeyCode() == KeyEvent.VK_LEFT){
+            player.left = false;
+        }
+    }
+}
+    
